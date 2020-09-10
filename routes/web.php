1bae5254cab0b523bcacb49ff2c58ab0eb241ea0
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-09-10 17:57:10 -0400

    Avances_(10-9-20)

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Image;
+
 
 Route::get('/', function () {
+    $images = Image::all();
+    foreach($images as $image){
+        /*echo('<pre>');
+        var_dump($image);*/
+        echo $image->image_path."<br>";
+        echo $image->description."<br>";
+        echo $image->user->name.' '.$image->user->surname."<br>";
+
+        if (count($image->comments)>=1) {
+            echo '<h4>Comentarios</h4>';
+            foreach($image->comments as $comment){
+                echo $comment->user->name.' '.$comment->user->surname.": ";
+                echo $comment->content.'<br>';
+            }
+        }
+        if (count($image->likes)>=1) {
+            echo 'Likes: '.count($image->likes);
+        }
+
+        echo "<hr>";
+    }
+    die();
     return view('welcome');
 });
