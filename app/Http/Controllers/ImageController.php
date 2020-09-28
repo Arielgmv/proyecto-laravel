bfaa189eae848dbf043174ef34773c42c13d31df
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-09-28 11:06:59 -0400

    Avances_(28-9-20)

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use App\Image;
 
 class ImageController extends Controller
 {
@@ -15,9 +18,32 @@ class ImageController extends Controller
     }
 
     public function save(Request $request){
-        var_dump($request->input('image_path'));
-        var_dump($request->input('description'));
-        die();
-        //echo('ok');
+        //Validación
+        $validate = $this->validate($request, [
+            'description' => 'required',
+            'image_path' => 'required|image'
+        ]);
+        //Recoger datos
+        $image_path = $request->file('image_path');
+        $description = $request->input('description');
+
+        //Asignar valores nuevo objeto
+        $user = \Auth::user();
+        $image = new Image();
+        $image->user_id = $user->id;
+        $image->description = $description;
+
+        //Subir fichero
+        if ($image_path) {
+            $image_path_name = time().$image_path->getClientOriginalName();
+            Storage::disk('images')->put($image_path_name, File::get($image_path));
+            $image->image_path = $image_path_name;
+        }
+
+        $image->save();
+
+        return redirect()->route('home')->with([
+            'message' => 'La foto ha sido subida correctamente'
+        ]);
     }
 }
