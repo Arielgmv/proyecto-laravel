a4c00841b1754aad836d6566ee55d349f24c8ade
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-09-29 18:42:59 -0400

    Avances_(29-9-20)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,24 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @include('includes.message')
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            @foreach ($images as $image)
+                <div class="card pub_image">
+                    <div class="card-header">
+                        @if ($image->user->image)
+                            <div class="container-avatar">
+                                <img src="{{ route('user.avatar',['filename'=>$image->user->image]) }}" class="avatar">
+                            </div>
+                        @endif
+                        <div class="data-user">
+                            {{$image->user->name.' '.$image->user->surname.' | @'.$image->user->nick}}
                         </div>
-                    @endif
+                    </div>
 
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
